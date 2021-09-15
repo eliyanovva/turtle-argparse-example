35071e19de3f3e6ec5cbe9767450fc096c386aa2
Teodora Petkova <67368977+eliyanovva@users.noreply.github.com>
2021-09-16 00:37:30 +0300

    Add files via upload

---
+++ shape.py
@@ -0,0 +1,13 @@
+import turtle
+import time
+
+def draw_shape( angle_degrees, sides, length = 50, pen_color = 'black', sleep_time = 3):
+    """
+    Draws a regular polygon with a specified number of sides, angle degree, and optional
+        length of the side, color of the pen and time for which the drawing remains on the screen.
+    """
+    turtle.pen(pencolor = pen_color)
+    for i in range(0, sides):
+        turtle.fd(length)
+        turtle.left(angle_degrees)
+    time.sleep(sleep_time)
