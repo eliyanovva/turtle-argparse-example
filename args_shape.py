ac21be9127609cbe9c08ce402ba3b33306ec4bd2
Teodora Petkova <67368977+eliyanovva@users.noreply.github.com>
2021-09-23 13:34:43 +0300

    Add files via upload

--- args_shape.py
+++ args_shape.py
@@ -7,22 +7,27 @@ from shape import draw_shape
 
 SIDES = 0
 ANGLE_DEGREES = 0
-LENGTH = 50
+LENGTH = 0
 PEN_COLOR = 'black'
 WAIT_TIME = 3
 
 # The ArgumentParser class is initialized.
-parser = argparse.ArgumentParser()
+parser = argparse.ArgumentParser(
+    description='Use this module to print your own shape!',
+    epilog='Thanks for stopping by!'
+    )
 
 # Optional arguments starting with '--' are added.
 # action = 'store_true' enables them to be called without giving them a value.
 parser.add_argument(
-    '--big', help = 'Makes the side of the shape 100 pixels.', action = 'store_true'    
+    '--big', help = 'Makes the side of the shape 100 pixels.'#, action = 'store_true'    
 )
 parser.add_argument(
     '--small', help = 'Makes the side of the shape 30 pixels.', action = 'store_true'
 )
-
+parser.add_argument(
+    '--length', help = 'Sets the length of the side of a shape to a custom value.', type = int
+)
 # Some more optional arguments, which will require a value:
 parser.add_argument(
     '--pen-color', help = 'Sets the color of the pen to a given color.'
@@ -36,30 +41,26 @@ parser.add_argument(
 )
 # Finally, this is a required argument.
 parser.add_argument(
-    'shape',
-    help = 'Prints the specified shape, which can be a SQUARE, a TRIANGLE, or a HEXAGON.'
+    'sides',
+    help = 'Specifies the selected shape, which can be a SQUARE, a TRIANGLE, or a HEXAGON.',
+    type = int
 )
 
 # pars_args checks the command line input and converts each argument to the appropriate type.
 # It returns a tuple of all arguments and their values.
 args = parser.parse_args()
+print(args)
 
 # The following lines define the behavior of the python module based on the CL arguments.
 # If an argument was not called, then it defaults to None.
-if args.shape == 'SQUARE':
-    SIDES = 4
-    ANGLE_DEGREES = 90
-elif args.shape == 'TRIANGLE':
-    SIDES = 3
-    ANGLE_DEGREES = 120
-elif args.shape == 'HEXAGON':
-    SIDES = 6
-    ANGLE_DEGREES = 60
+SIDES = args.sides
+ANGLE_DEGREES = 180 - ((SIDES-2)*180)/SIDES
 
-if args.big:
-    LENGTH = 100
-if args.small:
-    LENGTH = 30
+if not args.length:
+    LENGTH = 50 + args.big*50 + args.small*(-20)
+else:
+    LENGTH = args.length
+    
 if args.pen_color:
     PEN_COLOR = args.pen_color
 if args.time:
